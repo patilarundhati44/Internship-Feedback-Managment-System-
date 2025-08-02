7f13084f10e80dd510b1e752022ac718798fc1d3
patilarundati44 <patilarundhati44@gmail.com>
2025-08-02 18:41:09 +0530

    Initial Commit - Internship Feedback System

--- src/main/java/com/project/internshipfeedback/controller/HomeController.java
+++ src/main/java/com/project/internshipfeedback/controller/HomeController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController 
+
+{
 
     @GetMapping("/home")
-    public String homePage() {
-        return "home"; // Refers to home.html in templates
+    public String homePage() 
+    
+    {
+        return "home";
     }
 }
